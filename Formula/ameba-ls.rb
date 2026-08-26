21ea76575ebd16a13214218e3d62a689882b9a8b
Sijawusz Pur Rahnama <sija@sija.pl>
2026-08-26 13:05:52 +0200

    Update Ameba LS to version 0.2.0

--- Formula/ameba-ls.rb
+++ Formula/ameba-ls.rb
@@ -2,8 +2,8 @@ class AmebaLs < Formula
   desc "Language Server for Ameba - a static code analysis tool for Crystal"
   homepage "https://github.com/crystal-ameba/ameba-ls"
 
-  url "https://github.com/crystal-ameba/ameba-ls/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "adb7470bfc2d3e698f71ba88f5e45b01a1074184e4eb5459d2117c3f14bdc0c7"
+  url "https://github.com/crystal-ameba/ameba-ls/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "66b966a5c90c40e30e68002e82884048ef972a626b7a54b4dc6a4f977f93dfb0"
 
   license "MIT"
   head "https://github.com/crystal-ameba/ameba-ls.git", branch: "master"
